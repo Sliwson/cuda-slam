bfa25134803680c681bbedb0aa47a34fb493c646
Rogal27 <Rogal27@users.noreply.github.com>
2020-05-01 15:14:40 +0200

    Add paths to new clouds

--- source/cpumain.cpp
+++ source/cpumain.cpp
@@ -4,6 +4,10 @@
 constexpr float TEST_EPS = 1e-6f;
 constexpr int CLOUD_SIZE = 3000;
 const char* object_path = "data/bunny.obj";
+const char* object_path1 = "data/noise_00_bunny.off";
+const char* object_path2 = "data/noise_25_bunny.off";
+const char* object_path3 = "data/noise_50_bunny.off";
+const char* object_path4 = "data/bunny-decapitated.obj";
 
 int main()
 {
