6bb069add9e8f46e6ba617fb98a07f10843d825f
Mateusz Śliwakowski <mateusz_sliw@interia.pl>
2020-05-01 11:18:23 +0200

    Add simple timer class

---
+++ source/timer.cpp
@@ -0,0 +1,65 @@
+#include "timer.h"
+
+namespace Common
+{
+	std::shared_ptr<StageProperties> Timer::AddStage(const std::string& name)
+	{
+		if (auto foundStage = GetStage(name))
+			return foundStage;
+
+		auto stage = std::make_shared<StageProperties>(name);
+		stages.push_back(stage);
+		return stage;
+	}
+
+	void Timer::StartStage(const std::string& name)
+	{
+		auto stage = GetStage(name);
+		if (stage == nullptr)
+			stage = AddStage(name);
+
+		if (!stage->IsRunning)
+		{
+			stage->Begin = std::chrono::high_resolution_clock::now();
+			stage->IsRunning = true;
+		}
+	}
+
+	void Timer::StopStage(const std::string& name)
+	{
+		auto stage = GetStage(name);
+		if (stage == nullptr || !stage->IsRunning)
+			return;
+
+		const auto duration = std::chrono::high_resolution_clock::now() - stage->Begin;
+		const auto milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(duration);
+		stage->MilisecondsElpased += milliseconds;
+		stage->IsRunning = false;
+	}
+
+	void Timer::StageTimedCall(const std::string& name, std::function<void()> func)
+	{
+		StartStage(name);
+		func();
+		StopStage(name);
+	}
+
+	void Timer::PrintResults()
+	{
+		if (std::any_of(stages.begin(), stages.end(), [](auto prop) { return prop->IsRunning; }))
+			printf("One or more timers are still running!");
+
+		printf("%s results:\n", timerName.c_str());
+		for (auto stage : stages)
+			printf("%s -> %lldms\n", stage->Name.c_str(), static_cast<long long int>(stage->MilisecondsElpased.count()));
+	}
+
+	std::shared_ptr<StageProperties> Timer::GetStage(const std::string& name)
+	{
+		auto it = std::find_if(stages.begin(), stages.end(), [&name](auto prop) { return prop->Name == name; });
+		if (it != stages.end())
+			return *it;
+		else
+			return nullptr;
+	}
+}
---
+++ source/timer.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "_common.h"
+
+namespace Common {
+	struct StageProperties
+	{
+		StageProperties(const std::string& name) : Name(name) {}
+
+		std::string Name = "";
+		bool IsRunning = false;
+		std::chrono::time_point<std::chrono::high_resolution_clock> Begin;
+		std::chrono::milliseconds MilisecondsElpased = std::chrono::duration_values<std::chrono::milliseconds>::zero();
+	};
+
+	class Timer {
+	public:
+		Timer(const std::string& name = "Timer") : timerName(name) {}
+		Timer(const Timer&) = delete;
+		Timer& operator=(const Timer&) = delete;
+
+		std::shared_ptr<StageProperties> AddStage(const std::string& name);
+		void StartStage(const std::string& name);
+		void StopStage(const std::string& name);
+		void StageTimedCall(const std::string& name, std::function<void()> func);
+
+		void PrintResults();
+
+	private:
+
+		std::shared_ptr<StageProperties> GetStage(const std::string& name);
+		std::vector<std::shared_ptr<StageProperties>> stages;
+		std::string timerName = "";
+
+	};
+}
+
