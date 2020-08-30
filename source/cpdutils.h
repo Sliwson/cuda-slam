0694e7cde92949c02e398c59ddc8902ed4774b6a
Michal Rogala <gm.rogala@gmail.com>
2020-08-30 22:45:32 +0200

    Move probabilities counting using fgt to common

---
+++ source/cpdutils.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "coherentpointdrift.h"
+
+namespace CoherentPointDrift
+{
+	struct Probabilities
+	{
+		// The probability matrix, multiplied by the identity vector.
+		Eigen::VectorXf p1;
+		// The probability matrix, transposed, multiplied by the identity vector.
+		Eigen::VectorXf pt1;
+		// The probability matrix multiplied by the fixed(cloud before) points.
+		Eigen::MatrixXf px;
+		// The total error.
+		float error;
+	};
+}
+
+namespace CPDutils
+{
+	CoherentPointDrift::Probabilities ComputePMatrixWithFGT(
+		const std::vector<Common::Point_f>& cloudBefore,
+		const std::vector<Common::Point_f>& cloudTransformed,
+		const float& weight,
+		const float& sigmaSquared,
+		const float& sigmaSquaredInit);
+}
