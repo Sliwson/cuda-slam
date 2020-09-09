91bd00789e239a1db497e5432e73df425a9516ac
Michal Rogala <gm.rogala@gmail.com>
2020-09-10 00:54:19 +0200

    Use SetRandom function in RunCpuTests

--- source/cpu-slam/cpumain.cpp
+++ source/cpu-slam/cpumain.cpp
@@ -3,6 +3,7 @@
 #include "basicicp.h"
 
 #include "mainwrapper.h"
+#include "common.h"
 
 using namespace Common;
 
@@ -25,6 +26,7 @@ namespace {
 	int RunCpuTests()
 	{ 
 		srand(Tests::RANDOM_SEED);
+		Common::SetRandom();
 
 		const auto methods = { ComputationMethod::Icp, ComputationMethod::NoniterativeIcp, ComputationMethod::Cpd };
 		Tests::RunTestSet(GetSizesTestSet, GetCpuSlamResult, "sizes", methods);
